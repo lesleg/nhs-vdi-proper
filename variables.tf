4cca491407a2ac1bcf6d75af53f2a1391e6221fa
Adam Tallentire <adam.tallentire1@nhs.net>
2021-05-05 11:30:01 +0100

    DSP-21113 remove unneeded TF files

--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "cidr_block" {
-  default     = "10.0.0.0/16"
-  type        = string
-  description = "CIDR block for the VPC"
-}
--- vpc.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_vpc" "default" {
-  cidr_block           = var.cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
