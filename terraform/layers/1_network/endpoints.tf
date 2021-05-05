50cd8ab351405e50e4c8f20db94481dbdf9a1810
Adam Tallentire <adam.tallentire1@nhs.net>
2021-05-05 11:52:25 +0100

    DSP-21113 add VPC endpoint for connecting to RStudio

--- terraform/layers/1_network/endpoints.tf
+++ terraform/layers/1_network/endpoints.tf
@@ -3,6 +3,23 @@ resource "aws_vpc_endpoint" "s3" {
   service_name = "com.amazonaws.eu-west-2.s3"
 
   tags = {
-    Environment = "test"
+    Name        = "S3 endpoint"
+    environment = var.environment
+  }
+}
+
+resource "aws_vpc_endpoint" "rstudio" {
+  vpc_id       = aws_vpc.default.id
+  service_name = var.rstudio_endpoint_service_name
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id,
+    aws_subnet.private_c.id
+  ]
+
+  tags = {
+    Name        = "RStudio endpoint"
+    environment = var.environment
   }
 }
--- terraform/layers/1_network/vars/dev.tfvars
+++ terraform/layers/1_network/vars/dev.tfvars
@@ -1,2 +1,4 @@
 environment = "dev"
 account_id  = "092272783160"
+
+rstudio_endpoint_service_name = "com.amazonaws.vpce.eu-west-2.vpce-svc-0c1d9958828aad67f"
