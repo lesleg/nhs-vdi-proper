03389aeebf94b455fcaf1f45d394bffce1decb1c
Adam Tallentire <adam.tallentire1@nhs.net>
2021-05-11 18:41:56 +0100

    DSP-21245 correct IAM policy version string

--- terraform/layers/1_network/cloudtrail.tf
+++ terraform/layers/1_network/cloudtrail.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket" "cloudtrail" {
 
   policy = <<POLICY
 {
-    "Version": "2021-04-20",
+    "Version": "2012-10-17",
     "Statement": [
         {
             "Sid": "AWSCloudTrailAclCheck",
