15fff401fba69ae0e83251971f748f7a55219c18
adam.tallentire1 <adam.tallentire1@hscic.gov.uk>
2021-06-27 10:14:32 +0000

    DARE-222 Deploy network layer to access dev account

--- terraform/root_modules/1_network/variables.tf
+++ terraform/root_modules/1_network/variables.tf
@@ -1,7 +1,5 @@
 variable "environment" {}
 
-variable "account_id" {}
-
 variable "cidr_block" {
   default     = "10.0.0.0/16"
   type        = string
@@ -19,8 +17,5 @@ variable "private_subnet_b_cidr_block" {
 variable "private_subnet_c_cidr_block" {
   default = "10.0.2.0/24"
 }
-
-variable "rstudio_endpoint_service_name" {}
-variable "hue_endpoint_service_name" {}
 
 variable "s3_bucket_prefix" {}
