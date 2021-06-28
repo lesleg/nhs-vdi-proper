0cf71e65574e80966ca708592482de2c61470096
TAMO1 <tamo1@hscic.gov.uk>
2021-06-28 16:16:37 +0000

    DSP-22217 airflow endpoint

--- terraform/root_modules/1_network/endpoints.tf
+++ terraform/root_modules/1_network/endpoints.tf
@@ -3,7 +3,37 @@ resource "aws_vpc_endpoint" "s3" {
   service_name = "com.amazonaws.eu-west-2.s3"
 
   tags = {
-    Name        = "S3 endpoint"
-    environment = var.environment
+    Name = "S3 endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "airflow" {
+  vpc_id             = aws_vpc.default.id
+  vpc_endpoint_type  = "Interface"
+  service_name       = var.airflow_endpoint_service_name
+  security_group_ids = [aws_security_group.airflow_endpoint_sg.id]
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id,
+    aws_subnet.private_c.id,
+  ]
+
+  tags = {
+    Name = "Airflow endpoint"
+  }
+}
+
+resource "aws_security_group" "airflow_endpoint_sg" {
+  name        = "airflow-endpoint-${terraform.workspace}"
+  description = "Security Group for VPC Endpoint to Airflow"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.default.cidr_block]
   }
 }
