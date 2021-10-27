ef1cfb6b6e449712cc29095910c4b6a5a7e0e287
anle9 <andrew.leslie1@nhs.net>
2021-10-27 14:06:54 +0100

    CIDR ranges for HSCN added

--- terraform/root_modules/2_directory/directory.tf
+++ terraform/root_modules/2_directory/directory.tf
@@ -89,7 +89,7 @@ resource "aws_workspaces_ip_group" "HSCN" {
   description = "HSCN IP access control group"
 
   rules {
-    source      = "3.10.167.180/32"
+    source      = var.hscn_network_cidrs
     description = "HSCN IP access control group"
   }
 }
