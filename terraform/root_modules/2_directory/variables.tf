2b321eba5a0eb38e07b265f5fd8bae50cb48ada5
anle9 <andrew.leslie1@nhs.net>
2021-10-28 14:01:05 +0100

    Ammended comments

--- terraform/root_modules/2_directory/variables.tf
+++ terraform/root_modules/2_directory/variables.tf
@@ -9,10 +9,11 @@ variable "workspace_role_name" {
 }
 
 variable "hscn_network_cidrs" {
-  description = "https://nhsd-confluence.digital.nhs.uk/pages/viewpage.action?pageId=83080277"
+  description = "https://nhsd-confluence.digital.nhs.uk/x/VbTzB"
   type        = list(string)
 
   default = [
+    # Misc NHSD
     "193.84.224.0/23",
     "194.101.68.0/24",
     "194.101.82.0/24",
@@ -23,16 +24,21 @@ variable "hscn_network_cidrs" {
     "195.104.77.0/24",
     "195.143.82.0/24",
     "212.222.181.0/24",
+    # HSCN
     "62.172.169.0/25",
     "62.6.52.0/24",
+    # NHSD ForcePoint Cloud Proxy
     "85.115.52.0/24",
     "85.115.53.0/24",
     "85.115.54.0/24",
+    # AWS VPN
     "3.10.167.180/32",
     "3.10.37.54/32",
     "3.11.106.35/32",
+    # CNSP
     "208.127.192.0/21",
     "208.127.199.0/24",
+    # Leeds Hub
     "194.101.80.0/24",
     "155.231.226.0/24"
   ]
